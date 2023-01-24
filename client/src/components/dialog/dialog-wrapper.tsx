bf7b5a99cde6d6562482c1d38aad8d9c629b1b8f
Mikas Pupeikis <mikas.pup@gmail.com>
2023-01-24 20:37:34 +0200

    Dialog layout

--- client/src/components/dialog/dialog-wrapper.tsx
+++ client/src/components/dialog/dialog-wrapper.tsx
@@ -9,27 +9,26 @@ export const DialogWrapper: React.FC<Props> = () => {
 	const { open, setOpen } = useContext(DialogContext);
 
 	const classes = [
-		'fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full',
-		open ? '' : 'hidden',
+		'fixed mx-auto z-50 w-1/2 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full',
+		open ? '' : ' hidden',
 	];
 
 	const resetDialog = () => {
 		setOpen(false);
 	};
 
-	if (!open) return null;
-
 	return ReactDOM.createPortal(
 		<div className={classes.join('')}>
-			<div className="relative w-full h-full max-w-2xl md:h-auto">
+			<div className="relative w-full h-full">
 				<div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-					<div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-						<h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-							Terms of Service
+					<div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
+						<h3 className="text-xl font-medium text-gray-900 dark:text-white">
+							Small modal
 						</h3>
 						<button
 							type="button"
 							className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+							data-modal-hide="small-modal"
 						>
 							<svg
 								aria-hidden="true"
@@ -39,14 +38,15 @@ export const DialogWrapper: React.FC<Props> = () => {
 								xmlns="http://www.w3.org/2000/svg"
 							>
 								<path
-									fillRule="evenodd"
+									fill-rule="evenodd"
 									d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-									clipRule="evenodd"
+									clip-rule="evenodd"
 								></path>
 							</svg>
 							<span className="sr-only">Close modal</span>
 						</button>
 					</div>
+
 					<div className="p-6 space-y-6">
 						<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
 							With less than a month to go before the European Union enacts new
@@ -61,6 +61,7 @@ export const DialogWrapper: React.FC<Props> = () => {
 							could personally affect them.
 						</p>
 					</div>
+
 					<div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
 						<button
 							type="button"
@@ -71,7 +72,7 @@ export const DialogWrapper: React.FC<Props> = () => {
 						</button>
 						<button
 							type="button"
-							className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+							className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
 							onClick={resetDialog}
 						>
 							Decline
